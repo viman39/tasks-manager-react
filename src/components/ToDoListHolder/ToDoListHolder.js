fb81a601305025176e85a1373118e50c048afbf8
viman39 <vimanmanuel@gmail.com>
2021-09-24 10:11:48 +0300

    add css for tasks card and task done element

--- src/components/ToDoListHolder/ToDoListHolder.js
+++ src/components/ToDoListHolder/ToDoListHolder.js
@@ -1,25 +1,33 @@
+import styles from './ToDoListHolder.module.css'
+
 const ToDoListHolder = (props) => {
     const tasks = props.tasks
 
     if ( tasks.length === 0 ){
-        return <div>No tasks yet!</div>
+        return <div className={styles.card}>No tasks yet!</div>;
     } else {
         const markAsFinished = (event) => {
-            console.log(event)
-            props.markAsFinished(event.target.id)
+          props.markAsFinished(event.target.id);
         }
 
-        const filteredTasks = tasks.filter( task => { return task.done === false } )
+        const tasksDisplay = tasks.map((task) => (
+          <li
+            id={task.id}
+            key={task.id}
+            onClick={markAsFinished}
+            className={`${styles.task} ${
+              task.done === true && styles["task-done"]
+            }`}
+          >
+            {task.name} {task.done === true ? ' V' : ' X'}
+          </li>
+        ));
 
-        const tasksDisplay = filteredTasks.map(task =>
-            <div id={task.id} key={task.id} onClick={markAsFinished}>
-                {task.name} X
-            </div>
-        )
-
-        return <div>
-            {tasksDisplay}
-        </div>
+        return (
+          <div className={styles.card}>
+            <ul>{tasksDisplay}</ul>
+          </div>
+        );
     }
 }
 
